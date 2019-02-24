b4a0deab9e2ec875ff2c2b7ec2db0ad536deb6b5
VaniOFX <iebardarov@gmail.com>
2019-02-24 21:50:12 +0000

    fixed paths

--- app/static/client.js
+++ app/static/client.js
@@ -1,8 +1,8 @@
 var el = x => document.getElementById(x);
 
 function showPicker(inputId) {
-     el('file-input').click(); 
-     el('analyze-')
+     el('file-input').click();
+     el('analyze-button').style.display = "none";
     }
 
 function showPicked(input) {
@@ -12,21 +12,50 @@ function showPicked(input) {
         el('results-button').style.display = "none";
         el('analyze-button').style.display = "inline-block";
         el('analyze-button').innerHTML = "Analyze";
-
     }
     reader.readAsDataURL(input.files[0]);
 }
 
-function analyze() {
+function detect() {
     var uploadFiles = el('file-input').files;
     if (uploadFiles.length != 1){
          alert('Please select 1 file to analyze!');
          return;
     }
-    el('analyze-button').innerHTML = "<i class='fa fa-spinner fa-spin '></i> Analyzing";
+    
     var xhr = new XMLHttpRequest();
     var loc = window.location
     xhr.open('POST', `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`, true);
+    xhr.onerror = function() { alert (xhr.responseText); }
+    xhr.onload = function(e) {
+        if (this.readyState === 4) {
+            var response = JSON.parse(e.target.responseText);
+            var zipped = zip(response["cropped_items"], response["categories"])
+            zipped.forEach(function(e) {
+                res = document.createElement('img')
+                res.src = e[0]
+                res.id = e[1]
+                res.class = "img-thumbnail"
+                res.style.width = 200
+                res.onclick = analyze(res)
+                el("results-diplay").appendChild(res)
+              });
+            el("test-image").display = "none"
+        }
+        // el('analyze-button').style.display = 'none';
+    }
+    var fileData = new FormData();
+    fileData.append('file', uploadFiles[0]);
+    xhr.send(fileData);
+    el('analyze-button').innerHTML = "<i class='fa fa-spinner fa-spin '></i> Analyzing";
+
+}
+
+function analyze(pic) {
+    el('analyze-button').innerHTML = "<i class='fa fa-spinner fa-spin '></i> Fetching Results";
+    var xhr = new XMLHttpRequest();
+    var loc = window.location
+    xhr.open('POST', `${loc.protocol}//${loc.hostname}:${loc.port}/detect`, true);
     xhr.onerror = function() { alert (xhr.responseText); }
     xhr.onload = function(e) {
         if (this.readyState === 4) {
@@ -41,8 +70,14 @@ function analyze() {
         }
         el('analyze-button').innerHTML = 'Analyze';
     }
-    var fileData = new FormData();
-    fileData.append('file', uploadFiles[0]);
-    xhr.send(fileData);
+    var json = {"chosen_image": pic.src, "chosen_cat": parseInt(pic.id)};
+    xhr.send(JSON.stringify(json));
+
 }
+
+function zip(a, b) {
+    var arr = [];
+    for (var key in a) arr.push([a[key], b[key]]);
+    return arr;
+  }
 
