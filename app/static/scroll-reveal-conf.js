5df2f249c50493fbf5f9666e718258381b7bbdfd
Ivan <iebardarov@gmail.com>
2019-02-26 23:23:34 +0000

    improved visualisations

--- app/static/scroll-reveal-conf.js
+++ app/static/scroll-reveal-conf.js
@@ -13,13 +13,9 @@ sr.reveal('.showcase-right', {
     origin: 'right',
     distance: '300px'
 });
-sr.reveal('.showcase-btn', {
-    duration: 2000,
-    delay: 2000,
-    origin: 'bottom'
-});
 sr.reveal('#testimonial div', {
     duration: 2000,
+    delay: 1000,
     origin: 'bottom'
 });
 sr.reveal('.info-left', {
@@ -34,7 +30,12 @@ sr.reveal('.info-right', {
     distance: '300px',
     viewFactor: 0.2
 });
-
+sr.reveal('#results-display', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px',
+    viewFactor: 0.2
+}); 
 // Smooth Scrolling
 $(function () {
   $('a[href*="#"]:not([href="#"])').click(function () {
