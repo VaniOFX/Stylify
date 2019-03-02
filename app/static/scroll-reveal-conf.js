8083196beb5ec0e201185d2d594b0814799e4fa4
VaniOFX <iebardarov@gmail.com>
2019-03-02 23:31:30 +0000

    refactoring done

--- app/static/scroll-reveal-conf.js
+++ app/static/scroll-reveal-conf.js
@@ -1,14 +1,16 @@
+autosize(document.getElementById("form-comments"));
+
 window.sr = ScrollReveal();
 sr.reveal('.navbar', {
     duration: 2000,
     origin: 'bottom'
 });
-sr.reveal('.showcase-left', {
+sr.reveal('.home-view-left', {
     duration: 2000,
     origin: 'top',
     distance: '300px'
 });
-sr.reveal('.showcase-right', {
+sr.reveal('.home-view-right', {
     duration: 2000,
     origin: 'right',
     distance: '300px'
@@ -18,13 +20,13 @@ sr.reveal('#testimonial div', {
     delay: 1000,
     origin: 'bottom'
 });
-sr.reveal('.info-left', {
+sr.reveal('.div-left', {
     duration: 2000,
     origin: 'left',
     distance: '300px',
     viewFactor: 0.2
 });
-sr.reveal('.info-right', {
+sr.reveal('.div-right', {
     duration: 2000,
     origin: 'right',
     distance: '300px',
@@ -36,11 +38,11 @@ sr.reveal('#results-display', {
     distance: '300px',
     viewFactor: 0.2
 }); 
+
 // Smooth Scrolling
 $(function () {
   $('a[href*="#"]:not([href="#"])').click(function () {
-       if ($(this).attr("href") == "#myCarousel") return;
-       if ($(this).attr("href") == "#myGallery") return;
+       if ($(this).attr("href") == "#result-gellery") return;
        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
            var target = $(this.hash);
            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
